2d1d20dd56a2ac51b08418c27bc91d2b207448cf
Luis Mendieta <luis.mendieta@ultra.io>
2023-03-29 15:53:27 -0600

    feat: add modal for transaction builder

--- src/index.js
+++ src/index.js
@@ -5,40 +5,52 @@ import {
   disconnectedEvent,
 } from './connect';
 import {
+  createSmartContractTransaction,
+  fetchSmartContractActions,
+} from './smartContracts';
+import {
   signMessageExample,
   tokenTransferExample,
 } from './transactionExamples';
 import { stringifyValue } from './utils';
 
+const menuActions = [
+  { title: 'Connect', action: connectAction, disabled: false },
+  { title: 'Disconnect', action: disconnectAction, disabled: true },
+  {
+    title: 'Sign Transaction',
+    action: tokenTransferExample,
+    disabled: true,
+  },
+  { title: 'Sign Message', action: signMessageExample, disabled: true },
+];
+
+const helpLinks = [
+  {
+    title: 'Wallet Docs',
+    href: 'https://docs.ultra.io/blockchain/#/docs/ultra-wallet-extension/',
+  },
+  { title: 'Mainnet Explorer', href: 'https://explorer.mainnet.ultra.io/' },
+  { title: 'Testnet Explorer', href: 'https://explorer.testnet.ultra.io/' },
+  {
+    title: 'Testnet Account Creation',
+    href: 'https://faucet.testnet.app.ultra.io/',
+  },
+  { title: 'Download Ultra', href: 'https://ultra.io/download' },
+  { title: 'Discord community', href: 'https://discord.gg/WfJCN6YbGk' },
+];
+
 document.addEventListener('alpine:init', () => {
   Alpine.store('app', {
     blockchainAccount: '',
     terminalLines: [],
-    menuActions: [
-      { title: 'Connect', action: connectAction, disabled: false },
-      { title: 'Disconnect', action: disconnectAction, disabled: true },
-      {
-        title: 'Sign Transaction',
-        action: tokenTransferExample,
-        disabled: true,
-      },
-      { title: 'Sign Message', action: signMessageExample, disabled: true },
-      { title: 'Smart Contract Builder', action: () => {}, disabled: true },
-    ],
-    helpLinks: [
-      {
-        title: 'Wallet Docs',
-        href: 'https://docs.ultra.io/blockchain/#/docs/ultra-wallet-extension/',
-      },
-      { title: 'Mainnet Explorer', href: 'https://explorer.mainnet.ultra.io/' },
-      { title: 'Testnet Explorer', href: 'https://explorer.testnet.ultra.io/' },
-      {
-        title: 'Testnet Account Creation',
-        href: 'https://faucet.testnet.app.ultra.io/',
-      },
-      { title: 'Download Ultra', href: 'https://ultra.io/download' },
-      { title: 'Discord community', href: 'https://discord.gg/WfJCN6YbGk' },
-    ],
+    blockchainAPI: 'https://api.mainnet.ultra.io',
+    contracts: ['eosio.token', 'eosio.nft.ft'],
+    selectedContract: null,
+    contractActions: [],
+    selectedContractAction: null,
+    menuActions,
+    helpLinks,
     postTerminalLog(value) {
       const log = stringifyValue(value);
       this.terminalLines.push(log);
@@ -47,6 +59,39 @@ document.addEventListener('alpine:init', () => {
       this.menuActions.forEach((_, key) => {
         this.menuActions[key].disabled = !this.menuActions[key].disabled;
       });
+    },
+    async onContractChange(name) {
+      this.selectedContract = name;
+      this.contractActions = [];
+      this.contractActions = await fetchSmartContractActions(
+        this.selectedContract
+      );
+    },
+    async onContractActionChange(actionName) {
+      const index = this.contractActions.findIndex(
+        (action) => action.name === actionName
+      );
+      this.selectedContractAction = this.contractActions[index];
+    },
+    onSubmitContractForm(formData) {
+      const form = new FormData(formData);
+      const dataObject = {};
+      form.forEach((value, key) => {
+        const nestedField = key.split('@');
+        if (nestedField.length === 2) {
+          if (!dataObject[nestedField[0]]) {
+            dataObject[nestedField[0]] = {};
+          }
+          dataObject[nestedField[0]][nestedField[1]] = value;
+        } else {
+          dataObject[key] = value;
+        }
+      });
+      createSmartContractTransaction(
+        this.selectedContract,
+        this.selectedContractAction.name,
+        dataObject
+      );
     },
   });
 
---
+++ src/smartContracts.js
@@ -0,0 +1,40 @@
+import { Api, JsonRpc } from 'eosjs';
+import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
+
+const defaultPrivateKey = '5JtUScZK2XEp3g9gh7F8bwtPTRAkASmNrrftmx4AxDKD5K4zDnr'; // bob
+const signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
+
+export async function fetchSmartContractActions(account) {
+  const APIendpoint = Alpine.store('app').blockchainAPI;
+  const rpc = new JsonRpc(APIendpoint, { fetch });
+  const api = new Api({
+    rpc,
+    signatureProvider,
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder(),
+  });
+  const abi = await api.getContract(account);
+  return Array.from(abi.actions, ([_, value]) => value);
+}
+
+export async function createSmartContractTransaction(account, name, data) {
+  const tx = {
+    contract: account,
+    action: name,
+    data,
+  };
+  // ui update
+  Alpine.store('app').postTerminalLog(
+    `ultra.signTransaction(${JSON.stringify(tx)})`
+  );
+
+  try {
+    // ask wallet to sign the given transaction object
+    const response = await ultra.signTransaction(tx);
+    // ui update
+    Alpine.store('app').postTerminalLog(response);
+  } catch (error) {
+    // ui update
+    Alpine.store('app').postTerminalLog(error);
+  }
+}
